9b10d1afb119fc50f016f15ebbaba95f495c8589
xrudoga <defcw58@gmail.com>
2020-03-07 15:44:06 +0800

    Users Signup and Signin

---
+++ src/user/Signin.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom'
+import { signin, authenticate } from '../auth'
+
+const Signin = () => {
+    const [values, setValues] = useState({
+        email:'',
+        password:'',
+        error: '',
+        redirectToReferer: false,
+        loading: false
+    })
+    const { email, password, error, redirectToReferer, loading } = values
+    const redirectUser = () => {
+        if(redirectToReferer) {
+            return <Redirect to='/' />
+        }
+    }
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value })
+    }
+
+    const clickSubmit = event => {
+        event.preventDefault()
+        setValues({ ...values, error: false, loading: true  })
+        // console.log(user)
+        signin({ email, password})
+        .then(data => {
+            if(data.error) {
+                setValues({ ...values, error: data.error, loading: false })
+            } else {
+                // authenticate
+                authenticate(data, () => {
+                    setValues({
+                        ...values,
+                        redirectToReferer: true
+                    })
+                })
+                // redirect
+            }
+        })
+    }
+
+    const signinForm = () => (
+        <form>
+            <div className='form-group'>
+                <label className='text-muted'>Email</label>
+                <input
+                    onChange={handleChange('email')}
+                    type='email'
+                    className='form-control'
+                    value={email}
+                />
+            </div>
+            <div className='form-group'>
+                <label className='text-muted'>Password</label>
+                <input
+                    onChange={handleChange('password')}
+                    type='password'
+                    className='form-control'
+                    value={password}
+                />
+            </div>
+            <button
+                onClick={clickSubmit}
+                className='btn btn-raised btn btn-primary'
+            >
+                Submit
+            </button>
+        </form>
+    )
+    
+    const showLoading = () => (
+        loading && (
+            <div className='jumbotron text-center'>
+                <h2>Loading...</h2>
+            </div>
+        )
+    )
+
+    return (
+        <div className='container'>
+            <h2 className='mt-5 mb-5'>SignIn</h2>
+            {showLoading()}
+            <div
+                className='alert alert-danger'
+                style={{ display: error ? '' : 'none' }}
+            >
+                {error}
+            </div>
+            {signinForm()}
+            {redirectUser()}
+        </div>
+    )
+}
+
+export default Signin
---
+++ src/user/Signup.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+import { signup } from '../auth'
+
+const Signup = () => {
+    const [values, setValues] = useState({
+        name:'',
+        email:'',
+        password:'',
+        error: '',
+        open: false
+    })
+    const { name, email, password, error, open } = values
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value })
+    }
+
+    const clickSubmit = event => {
+        event.preventDefault()
+        setValues({ ...values, error: false })
+        // console.log(user)
+        signup({name, email, password})
+        .then(data => {
+            if(data.error) {
+                setValues({ ...values, error: data.error, open: false })
+            } else {
+                setValues({
+                    ...values,
+                    name: '',
+                    email: '',
+                    password: '',
+                    error: '',
+                    open: true
+                })
+            }
+        })
+    }
+
+    const signForm = () => (
+        <form>
+            <div className='form-group'>
+                <label className='text-muted'>Name</label>
+                <input
+                    onChange={handleChange('name')}
+                    type='text'
+                    className='form-control'
+                    value={name}
+                />
+            </div>
+            <div className='form-group'>
+                <label className='text-muted'>Email</label>
+                <input
+                    onChange={handleChange('email')}
+                    type='email'
+                    className='form-control'
+                    value={email}
+                />
+            </div>
+            <div className='form-group'>
+                <label className='text-muted'>Password</label>
+                <input
+                    onChange={handleChange('password')}
+                    type='password'
+                    className='form-control'
+                    value={password}
+                />
+            </div>
+            <button
+                onClick={clickSubmit}
+                className='btn btn-raised btn btn-primary'
+            >
+                Submit
+            </button>
+        </form>
+    )
+
+    return (
+        <div className='container'>
+            <h2 className='mt-5 mb-5'>Signup</h2>
+            <div
+                className='alert alert-danger'
+                style={{ display: error ? '' : 'none' }}
+            >
+                {error}
+            </div>
+            <div
+                className='alert alert-info'
+                style={{ display: open ? '' : 'none' }}
+            >
+                New account is successfully created. Please Sign In.
+            </div>
+            {signForm()}
+        </div>
+    )
+}
+
+export default Signup
