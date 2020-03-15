02ad8c4f64b83d0cf36a803440a3aa663e331208
xrudoga <defcw58@gmail.com>
2020-03-15 14:50:31 +0800

    Securing Social Login

--- src/user/SocialLogin.js
+++ src/user/SocialLogin.js
@@ -10,28 +10,21 @@ const SocialLogin = () => {
 
     const { redirectToReferrer } = values
 
-    // const responseGoogle = response => {
-    //     console.log(response);
-    // }
-
     const responseGoogle = response => {
         console.log(response)
-        const { googleId, name, email, imageUrl } = response.profileObj;
+        const tokenId = response.tokenId
         const user = {
-            password: googleId,
-            name: name,
-            email: email,
-            imageUrl: imageUrl
-        };
+            tokenId: tokenId
+        }
         // console.log("user obj to social login: ", user);
         socialLogin(user).then(data => {
             console.log("signin data: ", data);
             if (data.error) {
-                console.log("Error Login. Please try again..");
+                // console.log("Error Login. Please try again..")
             } else {
-                console.log("signin success - setting jwt: ", data);
+                console.log("social login response from api", data)
                 authenticate(data, () => {
-                    setValues({ redirectToReferrer: true });
+                    setValues({ redirectToReferrer: true })
                 })
             }
         })
